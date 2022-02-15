a5861fefe958454dff1d36cb947cdc4aa1d04199
Nestor Matias <solomatias321@gmail.com>
2022-02-15 17:08:51 +0100

    implenting repositories with mediatR

--- Minimal.Core/Services/MovieUseCases/CreateMovieHandler.cs
+++ Minimal.Core/Services/MovieUseCases/CreateMovieHandler.cs
@@ -1,13 +1,55 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Minimal.Core.DTOs;
+using Minimal.Core.Interfaces;
+using Minimal.Core.Models;
+using Minimal.Core.Requests;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Minimal.Core.Services.MovieUseCases
 {
-    public class CreateMovieHandler
+    public class CreateMovieHandler : IRequestHandler<CreateMovieRequest, BaseResponseDto<bool>>
     {
+        private readonly IRepository<Movie> _repository;
+        private readonly ILogger<CreateMovieHandler> _logger;
+        private readonly IMediator _mediator;
 
+        public CreateMovieHandler(IRepository<Movie> repository, ILogger<CreateMovieHandler> logger, IMediator mediator)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        public async Task<BaseResponseDto<bool>> Handle(CreateMovieRequest request, CancellationToken cancellationToken)
+        {
+            BaseResponseDto<bool> response = new BaseResponseDto<bool>();
+
+            try
+            {
+                var movie = new Movie
+                {
+                    Name = request.Name,
+                    Rating = request.Rating,
+                    IsDeleted = false,
+                    CreatedAt = DateTime.Now
+                };
+
+                await _repository.CreateAsync(movie);
+                response.Data = true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                response.Errors.Add("An error ocurred while creating the movie.");
+            }
+
+            return response;
+        }
     }
 }
---
+++ Minimal.Infrastructure/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Minimal.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimal.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
